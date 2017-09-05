e251ecd01fa9c925c3ea5345acee6359be2f0232
Devendra Vyas <vyas.devendr@univ-poitiers.fr>
2017-09-06 00:36:59 +0200

    Normalisiation Done | Improves driving a bit

--- utils.py
+++ utils.py
@@ -34,10 +34,11 @@ y_train = np.array(measurements)
 ##Temporarily declaring model here for testing. To be included in models.py
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape = (160,320,3)))
+model.add(Lambda(lambda x:x / 255.0 - 0.5, input_shape = (160,320,3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
